8fd03d37816e2f23f74b298ed2f632652fb12993
Pikaqu_tesla <arnoldzhang_inf@163.com>
2023-11-09 09:11:14 +0000

    Program a simple 3 layer neuronetwork

---
+++ 3layers.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+def sigmoid(a):
+
+    tmp = 1/(1+math.exp(-a))
+
+    return (tmp)
+
+def init_network():
+    network = {}
+    network["W1"] = np.array([0.1, 0.3, 0.5], [0.2, 0.4, 0.6])
+    network["b1"] = np.array([0.1, 0.2, 0.3])
+    network["W2"] = np.array([0.1, 0.4], [0.2, 0.5], [0.3, 0.6])
+    network["W3"] = np.array(([0.1, 0.3], [0.2, 0.4]))
+    network["b2"] = np.array(([0.1, 0.2]))
+    network["b3"] = np.array(([0.1, 0.2]))
+    return network
+
+def identify_finction(x)
+
+def forward(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+    a1 = np.dot( x, W1) +b1
+    z1 = sigmoid(a1)
+    a2= np.dot(a1,W2)+b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(a2,W3)+b3
+
+
